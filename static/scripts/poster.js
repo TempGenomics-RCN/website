cb7c1019901e2ea78ebcd00e74357b6e92d7811c
Rene Clark <31482962+rclark848@users.noreply.github.com>
2021-03-04 13:58:52 -0500

    Delete static directory

--- static/scripts/poster.js
+++
@@ -1,6 +0,0 @@
-var colorPick = document.getElementById('color-pick');
-var miniPage = document.querySelector('.minipage');
-
-colorPick.onchange = function(e) {
-    miniPage.style.backgroundColor = colorPick.value;
-}
--- static/scripts/tagclick.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#tagclick').onclick = function () {
-  const tagItems = document.querySelectorAll('.tag-link')
-  const wantedTag = document.querySelector('#selectedtag').value
-  for (let tagLink of tagItems) {
-    if (tagLink.textContent === wantedTag) {
-      window.location.href = tagLink.href
-    }
-  }
-}
